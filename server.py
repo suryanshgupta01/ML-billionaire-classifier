b6c6ad4838bfd2ecb64fcadb4aeda424b3f1c4c5
suryansh <suryanshgupta03@gmail.com>
2023-12-26 15:14:33 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,149 @@
+from flask import Flask, request, jsonify
+import joblib
+import json
+import numpy as np
+import cv2
+import pywt
+import cv2
+from flask_cors import CORS
+from PIL import Image
+import google.generativeai as genai
+from dotenv import load_dotenv
+import translators as ts
+from io import BytesIO
+import base64
+import os
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', "")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/classify_image', methods=['POST'])
+def classify_image():
+    image_data = request.form['image_data']
+    # print(image_data)
+    response = jsonify(classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/fetch_gemini', methods=['POST'])
+def fetch_gemini():
+    image = request.form['image_data']
+    img = base64_to_pillow_image(image)
+    modelAI = genai.GenerativeModel('gemini-pro-vision')
+    response = modelAI.generate_content(img)
+    answer = response.text
+    answer = ts.translate_text(answer,translator='google', to_language='en')
+    return jsonify({'gemini_response':answer})
+
+def base64_to_pillow_image(base64_string):
+    if base64_string.startswith('data:image'):
+        base64_data = base64_string.split(',')[1]
+    else:
+        base64_data = base64_string
+
+    # Decode base64 data
+    image_data = base64.b64decode(base64_data)
+
+    # Create a BytesIO object to simulate a file-like object
+    image_stream = BytesIO(image_data)
+
+    # Open the image using Pillow (PIL)
+    pillow_image = Image.open(image_stream)
+
+    return pillow_image
+
+__class_name_to_number = {}
+__class_number_to_name = {}
+__model = None
+
+def classify_image(image_base64_data, file_path=None):
+    imgs = get_cropped_image(file_path, image_base64_data)
+    result = []
+    for img in imgs:
+        scalled_raw_img = cv2.resize(img, (32, 32))
+        img_har = w2d(img, 'db1', 5)
+        scalled_img_har = cv2.resize(img_har, (32, 32))
+        combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_img_har.reshape(32 * 32, 1)))
+
+        len_image_array = 32*32*3 + 32*32
+
+        final = combined_img.reshape(1,len_image_array).astype(float)
+        result.append({
+            'class': __class_number_to_name[__model.predict(final)[0]],
+            'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_dictionary': __class_name_to_number
+            # 'testing':__model.predict_proba(final)
+        })
+
+    return result
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __class_name_to_number
+    global __class_number_to_name
+
+    with open('./class_dictionary.json', "r") as f:
+        __class_name_to_number = json.load(f)
+        __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
+
+    global __model
+    if __model is None:
+        with open('./saved_model.pkl', 'rb') as f:
+            __model = joblib.load(f)
+    print("loading saved artifacts...done")
+
+
+def get_cv2_image_from_base64_string(b64str):
+    encoded_data = b64str.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+def get_cropped_image(image_path, image_base64_data):
+    face_cascade = cv2.CascadeClassifier('./opencv/haarcascades/haarcascade_frontalface_default.xml')
+    
+    if image_path:
+        img = cv2.imread(image_path)
+    else:
+        img = get_cv2_image_from_base64_string(image_base64_data)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    cropped_faces = []
+    for (x,y,w,h) in faces:
+            roi_color = img[y:y+h, x:x+w]
+            cropped_faces.append(roi_color)
+    return cropped_faces
+
+
+def w2d(img, mode='haar', level=1):
+    imArray = img
+    #Datatype conversions
+    #convert to grayscale
+    imArray = cv2.cvtColor( imArray,cv2.COLOR_RGB2GRAY )
+    #convert to float
+    imArray =  np.float32(imArray)
+    imArray /= 255;
+    # compute coefficients
+    coeffs=pywt.wavedec2(imArray, mode, level=level)
+
+    #Process Coefficients
+    coeffs_H=list(coeffs)
+    coeffs_H[0] *= 0;
+
+    # reconstruction
+    imArray_H=pywt.waverec2(coeffs_H, mode);
+    imArray_H *= 255;
+    imArray_H =  np.uint8(imArray_H)
+
+    return imArray_H
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    load_saved_artifacts()
+    app.run(debug=True)
---
+++ website/index.js
@@ -0,0 +1,194 @@
+var Pic = null
+const baseURL = "http://127.0.0.1:5000"
+document.getElementById("file").addEventListener("change", function (event) {
+    event.preventDefault();
+    // var output = document.getElementById("file");
+    // var data = output.files[0];
+    // console.log(event.target.files[0])
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        Pic = reader.result;
+        document.getElementById("inputimage").src = Pic
+        document.getElementById('probability').innerHTML = "";
+        document.getElementById("billionaire_pics").innerHTML = "";
+        document.getElementById("billionaire").innerHTML = "";
+        execute_classify(Pic);
+        execute_gemini(Pic);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+});
+
+func_cleaned_data = (data) => {
+    cleaned_data = []
+    for (var entry of data) {
+        let maxi = Math.max(...entry.class_probability)
+        if (maxi > 50) {
+            cleaned_data.push(entry)
+        }
+    }
+    return cleaned_data
+}
+// not waiting for the button to be clicked
+// document.getElementById("submit").addEventListener("click", function (event) {
+const execute_classify = (Pic) => {
+    // event.preventDefault();
+    var url = baseURL + "/classify_image";
+    var formData = new FormData();
+    formData.append('image_data', Pic);
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        // console.log(data)
+        // data = data[0]
+        cleaned_data = func_cleaned_data(data)
+        var names = []
+        for (const entry of cleaned_data) {
+            var freshClass = capitalize(entry.class.replace('_', ' '))
+            names.push(freshClass)
+            filltable(freshClass, entry.class_probability)
+        }
+        if (names.length != 0) {
+            document.getElementById("billionaire").innerHTML = names.join(" & ");
+        } else {
+            document.getElementById("billionaire").innerHTML = "No Billionaire Found. Try again with different picture!";
+        }
+    });
+}
+
+function capitalize(word) {
+    wordArr = word.split(' ')
+    for (let i = 0; i < wordArr.length; i++) {
+        wordArr[i] = wordArr[i][0].toUpperCase() + wordArr[i].slice(1);
+    }
+    return wordArr.join(' ')
+
+}
+
+document.getElementById("gemini").addEventListener("click", function (event) {
+    event.preventDefault();
+    if (!Pic) {
+        alert("Please select an image first")
+        return
+    }
+    execute_gemini(Pic);
+})
+execute_gemini = (Pic) => {
+    document.getElementById("gemini").classList.toggle('nonclickable')
+    var url = baseURL + "/fetch_gemini"
+    var formData = new FormData();
+    formData.append('image_data', Pic);
+    var text = "";
+    var index = 0;
+    const textContainer = document.getElementById("info");
+    textContainer.innerHTML = "";
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data)
+            text = data.gemini_response;
+            updateText();
+            document.getElementById("gemini").classList.toggle('nonclickable')
+        }).catch(function (error) {
+            console.log('There was a problem with the fetch operation:', error);
+        });
+    function updateText() {
+        if (index < text.length) {
+            const span = document.createElement("span");
+            span.textContent = text[index];
+            textContainer.appendChild(span);
+
+            const cursor = document.createElement("div");
+            cursor.classList.add("cursor");
+            textContainer.appendChild(cursor);
+
+            index++;
+            // Remove the cursor after a short delay (adjust as needed)
+            setTimeout(() => {
+                textContainer.removeChild(cursor);
+            }, 10);
+            requestAnimationFrame(updateText);
+        }
+    }
+
+};
+
+const allbillionaires = document.getElementById("allbillionaires")
+const dict = new Map([
+    ["bernard arnault", 0],
+    ["bill gates", 1],
+    ["elon musk", 2],
+    ["gautam adani", 3],
+    ["jeff bezos", 4],
+    ["mark zuckerberg", 5],
+    ["mukesh ambani", 6],
+    ["warren buffett", 7]
+]);
+
+dict.forEach((value, key) => {
+    // console.log(value, key)
+    var url = "https://en.wikipedia.org/wiki/"
+    const single = document.createElement("div");
+    single.classList.add("make_column");
+    const image = document.createElement("img");
+    const link = document.createElement("a");
+    link.href = url + capitalize(key.replace('_', ' '));
+    link.setAttribute('target', '_blank');
+    link.appendChild(image);
+    const name = document.createElement("span");
+    image.src = "../testing_images/" + key + " (1).jpg";
+    name.textContent = capitalize(key);
+    single.appendChild(link);
+    single.appendChild(name);
+    allbillionaires.appendChild(single);
+})
+
+
+const tables = document.getElementById("probability");
+const filltable = (class1, prob) => {
+    const image = document.createElement("img");
+    image.src = "../testing_images/" + class1 + " (1).jpg";
+    document.getElementById("billionaire_pics").appendChild(image);
+    const table = document.createElement("table");
+    const tableHead = document.createElement("thead");
+    const tableRow = document.createElement("tr");
+    const nameHeader = document.createElement("th");
+    const probabilityHeader = document.createElement("th");
+
+    nameHeader.textContent = "Name";
+    probabilityHeader.textContent = "Probability";
+
+    tableRow.appendChild(nameHeader);
+    tableRow.appendChild(probabilityHeader);
+    tableHead.appendChild(tableRow);
+    table.appendChild(tableHead);
+
+    dict.forEach((value, item) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const probabilityCell = document.createElement("td");
+        nameCell.textContent = capitalize(item);
+        probabilityCell.textContent = prob[value];
+        row.appendChild(nameCell);
+        row.appendChild(probabilityCell);
+        if (capitalize(item) == class1) {
+            row.classList.add("selected")
+        }
+        table.appendChild(row);
+    })
+
+    tables.appendChild(table);
+    // document.getElementById("probability").appendChild(table);
+}
